d74d9db234eecca9818958c3d644e9f50fba6433
Randy Steele <steelerx155@gmail.com>
2019-10-12 15:05:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  remove_element_from_start_of_array = ["array"]
+  remove_element_from_start_of_array = ["array", "wow"]
   array = remove_element_from_start_of_array.shift
 end
 

49dc1f8a03863632c1ef4416b76aaa09882fdddc
Randy Steele <steelerx155@gmail.com>
2019-10-12 15:00:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   remove_element_from_start_of_array = ["array"]
-  array = remove_element_from_start_of_array.shift
+  "array" = remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
